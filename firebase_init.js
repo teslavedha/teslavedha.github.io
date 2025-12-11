73cefd011e648660a0cd35092d12e65edde97ce1
teslavedha <teslavedha@gmail.com>
2025-12-11 18:46:52 +0530

    Update firebase_init.js

--- firebase_init.js
+++ firebase_init.js
@@ -1,15 +1,21 @@
-<!-- firebase_init.js -->
-<script src="https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.7.0/firebase-auth-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore-compat.js"></script>
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-<script>
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // 🔴 PASTE YOUR FIREBASE CONFIG HERE ONLY ONCE
+  apiKey: "AIzaSyAF616w_YOJQiYICXQ8RxuEp4bDRzZPQuk",
+  authDomain: "teslavedha-58fd3.firebaseapp.com",
+  projectId: "teslavedha-58fd3",
+  storageBucket: "teslavedha-58fd3.firebasestorage.app",
+  messagingSenderId: "647191590052",
+  appId: "1:647191590052:web:f9c5696b95b2e29a892231",
+  measurementId: "G-DHXVT7L8TM"
 };
 
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
-const db   = firebase.firestore();
-</script>
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
