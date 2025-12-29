36ebb3caa3668e50a0355f38c01f2b97c8a43379
teslavedha <teslavedha@gmail.com>
2025-12-29 09:34:23 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,23 +1,21 @@
-<script type="module">
-  // Firebase core
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
+// Firebase core
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 
-  // Teslavedha Learning Firebase config
-  const firebaseConfig = {
-    apiKey: "AIzaSyAnDwmnfIZWFdyWD50Yjab3HYf-xgMht-I",
-    authDomain: "teslavedha-learning.firebaseapp.com",
-    projectId: "teslavedha-learning",
-    storageBucket: "teslavedha-learning.appspot.com",
-    messagingSenderId: "217735003172",
-    appId: "1:217735003172:web:6700e0118e65400f07ecb1"
-  };
+// Teslavedha Learning Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyAnDwmnfIZWFdyWD50Yjab3HYf-xgMht-I",
+  authDomain: "teslavedha-learning.firebaseapp.com",
+  projectId: "teslavedha-learning",
+  storageBucket: "teslavedha-learning.appspot.com",
+  messagingSenderId: "217735003172",
+  appId: "1:217735003172:web:6700e0118e65400f07ecb1"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-  // Make services globally available
-  window.auth = getAuth(app);
-  window.db = getFirestore(app);
-</script>
+// Export services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
