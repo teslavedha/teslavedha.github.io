c28e684f50011b882df48b7fcb3845bcf024b66d
teslavedha <teslavedha@gmail.com>
2025-12-30 07:05:25 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,8 @@
-// Firebase core
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 
-// Teslavedha Learning Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyAnDwmnfIZWFdyWD50Yjab3HYf-xgMht-I",
+  apiKey: "AIzaSyAndwmnFIZWFdyWD50Yjab3HYf-xgMht-I",
   authDomain: "teslavedha-learning.firebaseapp.com",
   projectId: "teslavedha-learning",
   storageBucket: "teslavedha-learning.appspot.com",
@@ -13,9 +10,5 @@ const firebaseConfig = {
   appId: "1:217735003172:web:6700e0118e65400f07ecb1"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export services
 export const auth = getAuth(app);
-export const db = getFirestore(app);
