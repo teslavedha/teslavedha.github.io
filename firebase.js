3855df35e1d0b57f831191c0ef6afa489bd21d4b
teslavedha <teslavedha@gmail.com>
2025-12-30 07:35:42 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,14 @@
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "COPY FROM Project Settings → Web App → Config",
-  authDomain: "COPY FROM SAME PLACE",
-  projectId: "COPY FROM SAME PLACE",
-  storageBucket: "COPY FROM SAME PLACE",
-  messagingSenderId: "COPY FROM SAME PLACE",
-  appId: "COPY FROM SAME PLACE"
+  apiKey: "AIzaSyAnDwmnfIZWFdyWD50Yjab3HYf-xgMht-I",
+  authDomain: "teslavedha-learning.firebaseapp.com",
+  projectId: "teslavedha-learning",
+  storageBucket: "teslavedha-learning.firebasestorage.app",
+  messagingSenderId: "217735003172",
+  appId: "1:217735003172:web:6700e0118e65400f07ecb1"
 };
 
 const app = initializeApp(firebaseConfig);
