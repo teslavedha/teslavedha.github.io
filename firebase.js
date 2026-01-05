b26fdbbcf5497f1bd094e0e4d9f1016c6e5f8e5d
teslavedha <teslavedha@gmail.com>
2026-01-05 10:57:08 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAnDwmnfIZWFdyWD50Yjab3HYf-xgMht-I",
   authDomain: "teslavedha-learning.firebaseapp.com",
   projectId: "teslavedha-learning",
-  storageBucket: "teslavedha-learning.firebasestorage.app",
+  storageBucket: "teslavedha-learning.appspot.com",
   messagingSenderId: "217735003172",
   appId: "1:217735003172:web:6700e0118e65400f07ecb1"
 };
