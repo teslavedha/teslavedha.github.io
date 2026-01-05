3296da2b7e083f3fedb99c2c64d82c9facc84fa7
teslavedha <teslavedha@gmail.com>
2026-01-05 11:16:17 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,20 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+<!-- firebase.js -->
+<script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAnDwmnfIZWFdyWD50Yjab3HYf-xgMht-I",
-  authDomain: "teslavedha-learning.firebaseapp.com",
-  projectId: "teslavedha-learning",
-  storageBucket: "teslavedha-learning.appspot.com",
-  messagingSenderId: "217735003172",
-  appId: "1:217735003172:web:6700e0118e65400f07ecb1"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyAnDwmnfIZWFdyWD50Yjab3HYf-xgMht-I",
+    authDomain: "teslavedha-learning.firebaseapp.com",
+    projectId: "teslavedha-learning",
+    storageBucket: "teslavedha-learning.appspot.com",
+    messagingSenderId: "217735003172",
+    appId: "1:217735003172:web:6700e0118e65400f07ecb1"
+  };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+  const app = initializeApp(firebaseConfig);
+
+  window.auth = getAuth(app);
+  window.db = getFirestore(app);
+</script>
